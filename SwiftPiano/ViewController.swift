81de7fa91fe649ed972b6545e26f66c003ca0bd0
Deepak <deepak@Macbooks-MacBook-Pro.local>
2017-02-26 00:52:25 +0530

    Committing code for Swift Piano

--- SwiftPiano/ViewController.swift
+++ SwiftPiano/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    let url:NSURL = Bundle.main.url(forResource: "C3", withExtension: "mp3")! as NSURL
+    //var PianoSoundC3 = NSURL(fileURLWithPath: Bundle.main.path(forResource: "C3",ofType: "mp3")!)
+    var audioPlayerC3 = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        do{
+            try audioPlayerC3 = AVAudioPlayer(contentsOf: url as URL)
+            audioPlayerC3.prepareToPlay()
+        }catch{}
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func C3(_ sender: UIButton) {
+        audioPlayerC3.currentTime=0;
+        audioPlayerC3.play()
+    }
 
 }
 
